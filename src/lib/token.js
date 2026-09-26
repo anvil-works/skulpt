691656e48cf47993e16a75e023be2cb02c2cd2ac
stu <stucork@gmail.com>
2026-09-26 19:06:12 +0800

    Keep shared stdlib wrappers compatible with legacy runtimes

--- src/lib/token.js
+++ src/lib/token.js
@@ -1,7 +1,42 @@
 /* Implementation of the Python token module */
 
 var $builtinmodule = function (name) {
-    return modernTokenModule();
+    var mod = {};
+
+    mod.__file__ = new Sk.builtin.str("/src/lib/token.py");
+
+    // The stdlib can be shared by old and new runtime builds during rollout.
+    if (Sk["$scanSource"]) {
+        return modernTokenModule();
+    }
+    const tok_name_values = [];
+    for (let token in Sk.token.tok_name) {
+        const token_name = Sk.token.tok_name[token].slice(2);
+        const token_num = parseInt(token, 10);
+
+        tok_name_values.push(Sk.ffi.remapToPy(token_num));
+        tok_name_values.push(Sk.ffi.remapToPy(token_name));
+
+        mod[token_name] = Sk.ffi.remapToPy(token_num);
+    }
+    mod.tok_name = new Sk.builtin.dict(tok_name_values);
+
+    mod.ISTERMINAL = new Sk.builtin.func(function (token) {
+        Sk.builtin.pyCheckArgsLen("ISTERMINAL", arguments.length, 1, 1);
+        return Sk.token.ISTERMINAL(Sk.ffi.remapToJs(token));
+    });
+
+    mod.ISNONTERMINAL = new Sk.builtin.func(function (token) {
+        Sk.builtin.pyCheckArgsLen("ISNONTERMINAL", arguments.length, 1, 1);
+        return Sk.token.ISNONTERMINAL(Sk.ffi.remapToJs(token));
+    });
+
+    mod.ISEOF = new Sk.builtin.func(function (token) {
+        Sk.builtin.pyCheckArgsLen("ISEOF", arguments.length, 1, 1);
+        return Sk.token.ISEOF(Sk.ffi.remapToJs(token));
+    });
+
+    return mod;
 };
 
 // Generated from CPython v3.14.3 Lib/token.py (PSF license).
